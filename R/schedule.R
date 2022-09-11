a587af89b09e4b5e9553250243b4fab705594fcf
domwoolf <domwoolf@gmail.com>
2022-09-11 19:22:47 -0400

    skeleton of schedule function

---
+++ R/schedule.R
@@ -0,0 +1,18 @@
+# Schedule.R
+#
+# contains functions to construct DayCent schedule files from spatial data
+#
+
+#' Create a DayCent Schedule file
+#'
+#' Used for side effect of writing a schedule file based on spatial data.
+#'
+#' This function creates a schedule file for a specified scenario and crop,
+#' using spatial climate, crop calendar, crop management, and soil data.
+#'
+#' @param
+#'
+#' @returns invisibly returns boolean indicating whether file was written succesfully.
+create_sched = function() {
+  print("Hello, world!")
+}
