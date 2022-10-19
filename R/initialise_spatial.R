110e4d2aa7298a4b467806309911cb52bc1af3ac
Shelby <shelby@XPS-8930>
2022-10-19 15:33:42 -0400

    Updated cell_data script and created sql extract file

--- R/initialise_spatial.R
+++ R/initialise_spatial.R
@@ -28,6 +28,7 @@ create_cell_data_csu_table = function() {
   names(min_N)          = 'fertN.amt'
 
   org_N                 = org_N[[165]]
+  org_N                 = org_N/10 # g m-2
   names(org_N)          = 'orgN.amt'
 
   names(org_CN)         = 'orgCN.ratio'
@@ -36,7 +37,9 @@ create_cell_data_csu_table = function() {
 
   # load gridid, irrigation table
   gridid.dt       = fread(paste(pkg.env$gis_path, 'run_seq_with_pset.csv',          sep='/'))
+  # add in run_seq which corresponds to the cohort
   gridid.dt       = gridid.dt[, run_seq := NULL]
+
 
   # create a vector of rotated grid cell numbers needed for weather file script
   gridid.xy       = setDT(as.data.frame(xyFromCell(org_CN, gridid.dt[,gridid])))
@@ -49,12 +52,20 @@ create_cell_data_csu_table = function() {
   # add rotated.gridid to gridid.dt
   gridid.dt[, gridid.rotated := rotated.gridid]
 
+  # add pset info
+  pset.dt         = fread(paste(pkg.env$gis_path, 'pset_values_maiz_00s.csv',       sep='/'))
+  pset.dt         = pset.dt[, pset_seq := NULL]
+  gridid.dt       = gridid.dt[pset.dt, on = .(pset_id = pset_id)]
+  gridid.dt       = gridid.dt[!is.na(gridid),]
+  setorder(gridid.dt, gridid)
+  setcolorder(gridid.dt, c('gridid', 'gridid.rotated', 'regionid', 'crop', 'irr', 'pset_id'))
+
   # add scenario column, filter, and order
   scenario.col = gridid.dt[, .('scenario' = rep(c('conv','res','ntill','ccg','ccl','rewild'))), by = irr]
   gridid.dt = unique(gridid.dt[scenario.col, on = .(irr = irr), by = .EACHI, allow.cartesian = TRUE])
   setorder(gridid.dt, gridid)
-  gridid.dt[irr != 1 | scenario != 'rewild',]
-  setcolorder(gridid.dt, c('gridid', 'gridid.rotated', 'regionid', 'pset_id', 'crop', 'irr'))
+  gridid.dt = gridid.dt[irr != 1 | scenario != 'rewild',]
+  setcolorder(gridid.dt, c('gridid', 'gridid.rotated', 'regionid', 'crop', 'irr','scenario','pset_id'))
 
   # merge rasters and create data.table filtered for gridid
   all_raster      = c(crop_calendar, min_N, org_N, org_CN, residue_return)
@@ -68,13 +79,14 @@ create_cell_data_csu_table = function() {
   raster_table[is.nan((fertN.amt)), fertN.amt := 0][is.nan(orgN.amt), orgN.amt := 0][is.nan(orgCN.ratio), orgCN.ratio := 0][is.nan(res.rtrn.amt), res.rtrn.amt := 0]
   raster_table[, plant.date := round(plant.date, digits = 0)][, harvest.date := round(harvest.date, digits = 0)]
   raster_table[, fertN.amt := round(fertN.amt, digits = 1)][, orgN.amt := round(orgN.amt, digits = 1)][, orgCN.ratio := round(orgCN.ratio, digits = 1)]
+  raster_table[, res.rtrn.amt := round(res.rtrn.amt, digits = 2)]
 
-  # what to do about plant/harv with NaN? remove? or use focal?
-  # raster_table_var  = names(raster_table)
-  NaNdt = raster_table[is.nan(plant.date),] # loss of 539 grid cells; 1.6% of grid cells
+  raster_table = raster_table[complete.cases(raster_table)]
 
   # join DT
-  main_table = gridid.dt[raster_table, on = .(gridid = cell)]
+  main_table   = gridid.dt[raster_table, on = .(gridid = cell)]
+  setcolorder(main_table, c('gridid', 'gridid.rotated', 'x', 'y','regionid', 'crop', 'irr','scenario','pset_id'))
+  setorder(main_table, gridid, scenario)
 
   return(main_table)
   fwrite(main_table, paste(pkg.env$out_path,"cell_data_table_csu.csv", sep = "/")) #csv
@@ -94,6 +106,7 @@ create_cell_data_csu_table = function() {
 #' @export
 create_cell_data_table = function() {
   # create rasters
+  crop_area      = rast(paste(pkg.env$gis_path, 'spam_crop_area_agg_.tif',            sep='/'))
   crop_calendar  = rast(paste(pkg.env$gis_path, 'crop_calendar.tif',                 sep='/'))
   min_N          = rast(paste(pkg.env$gis_path, 'fertilizer-n_by_crop.tif',          sep='/'))
   org_N          = rast(paste(pkg.env$gis_path, 'manure-n_cropland.tif',             sep='/'))
@@ -104,7 +117,11 @@ create_cell_data_table = function() {
   phh2o          = rast(paste(pkg.env$gis_path, 'phh2o_0-100cm_mean_agg.tif',        sep='/'))
   sand           = rast(paste(pkg.env$gis_path, 'sand_0-100cm_mean_agg.tif',         sep='/'))
   soc            = rast(paste(pkg.env$gis_path, 'soc_0-100cm_mean_agg.tif',          sep='/'))
+
   # tidy variables, names, and subset for maize-only
+  crop_area            = crop_area['MAIZ']
+  names(crop_area)     = c('total_ha', 'irr_ha', 'rain_ha')
+
   crop_calendar        = crop_calendar['Maize']
   crop_calendar        = crop_calendar[[c(3,7)]]
   names(crop_calendar) = c('plant.date','harvest.date')
@@ -127,50 +144,52 @@ create_cell_data_table = function() {
   sand                  = sand[[c(1,3,2,5,4)]]
   soc                   = soc[[c(1,3,2,5,4)]]
 
-  # UPDATE - create rotated for all cell - no filtering
+  # merge rasters and create data.table
+  all_raster      = c(crop_area, crop_calendar, min_N, org_N, org_CN, residue_return, bdod, clay, phh2o, sand, soc)
+  raster_table    = as.data.frame(all_raster, xy = TRUE, cells = TRUE, na.rm = FALSE)
+  raster_table    = setDT(raster_table)
+
+  # code some variable NaN to 0
+  raster_table[is.nan((fertN.amt)), fertN.amt := 0][is.nan(orgN.amt), orgN.amt := 0][is.nan(orgCN.ratio), orgCN.ratio := 0][is.nan(res.rtrn.amt), res.rtrn.amt := 0]
+
+  # round variables
+  raster_table[, plant.date := round(plant.date, digits = 0)][, harvest.date := round(harvest.date, digits = 0)]
+  raster_table[, fertN.amt := round(fertN.amt, digits = 1)][, orgN.amt := round(orgN.amt, digits = 1)][, orgCN.ratio := round(orgCN.ratio, digits = 1)]
+  raster_table[, total_ha := round(total_ha, digits = 2)][, irr_ha := round(irr_ha, digits = 2)][, rain_ha := round(rain_ha, digits = 2)]
+  round(raster_table[, c(13:37)], digits = 2)
+
+  # filter for crop area > 0, filter out NaN soil variables
+  raster_table    = raster_table[total_ha > 0,] # 30,058
+  raster_table    = raster_table[complete.cases(raster_table)] # drops to 13,063; a lot of NaN soil grids data
+
   # create a vector of rotated grid cell numbers needed for weather file script
-  gridid.xy       = setDT(as.data.frame(xyFromCell(org_CN, gridid.dt[,gridid])))
+  gridid.xy       = raster_table[, .(x,y)]
   gridid.xy.rot   = gridid.xy
   gridid.xy.rot$x = (gridid.xy.rot$x + 360) %% 360 # http://www.idlcoyote.com/map_tips/lonconvert.html
   org_CN.rot      = rotate(org_CN, left = FALSE)
 
   rotated.gridid  = cellFromXY(org_CN.rot, gridid.xy.rot)
 
-  # add rotated.gridid to gridid.dt
-  gridid.dt[, gridid.rotated := rotated.gridid]
-  setcolorder(gridid.dt, c('gridid', 'gridid.rotated', 'regionid', 'pset_id', 'crop', 'irr'))
+  # create gridid table, add scenarios, filter, and order
+  gridid.dt       = data.table(gridid = raster_table[,cell], gridid.rotated = rotated.gridid, crop = 'maize')
+  irr.gridid.dt   = gridid.dt[, .('irr' = rep(c(1,0))), by = gridid]
+  gridid.dt       = gridid.dt[irr.gridid.dt, on = .(gridid = gridid), by = .EACHI, allow.cartesian = TRUE]
 
-  # merge rasters and create data.table filtered for gridid
-  all_raster      = c(crop_calendar, min_N, org_N, org_CN, residue_return,
-                      bdod, clay, phh2o, sand, soc)
-  raster_table    = as.data.frame(all_raster, xy = TRUE, cells = TRUE, na.rm = FALSE)
-  raster_table    = setDT(raster_table)
-  unique.gridid   = sort(unique(gridid.dt$gridid))
+  scenario.col    = gridid.dt[, .('scenario' = rep(c('conv','res','ntill','ccg','ccl','rewild'))), by = irr]
+  gridid.dt       = unique(gridid.dt[scenario.col, on = .(irr = irr), by = .EACHI, allow.cartesian = TRUE])
 
-  raster_table = raster_table[cell %in% unique.gridid,]
+  setorder(gridid.dt, gridid)
+  gridid.dt       = gridid.dt[irr != 1 | scenario != 'rewild',]
 
-  # raster_table_var  = names(raster_table)
+  # join DT
+  main_table      = gridid.dt[raster_table, on = .(gridid = cell)]
 
-  # hybrid DT (each row = scenario for gridid)
-  # code NA to 0 (for most continuous variables, double check this)
-  #
-
-  # # wide to long format
-  # main_table     = melt(main_table, id.vars = c("cell"),
-  #                        measure.vars = main_table_var[-1])
-  # # group variables by cell
-  # main_table     = main_table[order(rank(cell))]
-  # # round plant and harvest dates
-  # main_table[variable %in% names(crop_calendar)[c(3,7,14,18,25,29,36,40)], round(value, digits=0)]
-
-  # use focal to align crop_area and crop_cal?
-  # keep these rotated cell numbers, could add row with main cell # and the unrotated cell #
-  # consider deleting NaN crop_area cells (but hold off until aligned with Yi's sims)
-  # need to add a 'crop_code' level in variable column
+  # remove non-irrigated cells
+  main_table      = main_table[irr!=0 | irr_ha != 0,]
 
   return(main_table)
-  fwrite(main_table, paste(pkg.env$out_path,"cell_data_table.csv", sep = "/")) #csv
-  save(main_table, file = paste(pkg.env$out_path,"cell_data_table.RData", sep = "/")) #rda
+  fwrite(main_table, paste(pkg.env$out_path,"cell_data_table_spam.csv", sep = "/")) #csv
+  save(main_table, file = paste(pkg.env$out_path,"cell_data_table_spam.RData", sep = "/")) #rda
 }
 
 # #' This function tests if crop area is allocated to winter or spring wheat where wheat is grown.
