2b2e973c0587800391f717ce7744af332e326455
Shelby <shelby@XPS-8930>
2022-11-07 15:50:45 -0500

    updated site.R function

--- R/site.R
+++ R/site.R
@@ -13,10 +13,11 @@
 #' @param .irr Integer, irrigation level
 #' @param cell_data Data table, contains spatial data associated with each cell and site.100 file
 #' @export
-create_site = function(cell, .irr, cell_data, cell_data_site = copy(cell_data_site)){
-  site.file     = paste(.gridid, unique(cell_data[gridid %in% .gridid & irr %in% .irr, run_seq]), 'site.100', sep = '_')
-  site.100      = cell_data_site[gridid %in% .gridid &
-                                   irr    %in% .irr, site100]
+create_site = function(.gridid, .runseq, .irr, cell_data_site = copy(cell_data_site)){
+  site.file     = paste(.gridid, .run_seq, 'site.100', sep = '_')
+  site.100      = cell_data_site[gridid    %in% .gridid &
+                                   irr     %in% .irr  &
+                                   run_seq %in% .runseq, site100]
   fwrite(list(site.100), paste(pkg.env$tmp_path, site.file, sep = '/'), quote = FALSE)
   return(site.file)
 }
