b45e9b9f2c4621aa68a6afe02691b4c098c0e0c3
Shelby <shelby@XPS-8930>
2022-11-09 09:54:42 -0500

    minor changes to data-raw files

--- data-raw/cell_data_template-csv.R
+++ data-raw/cell_data_template-csv.R
@@ -1,6 +1,6 @@
-## code to prepare `cell_data_table_csu.csv` dataset as .rda goes here
+## code to prepare `cell_data_table_csu.csv` dataset as multiple .rda goes here
 
 library(data.table)
-cell_data = fread('cell_data_table_csu.csv')
+cell_data = fread('data-raw/cell_data_table_csu.csv')
 
 usethis::use_data(cell_data, overwrite = TRUE)
--- data-raw/schedule_template-csv.R
+++ data-raw/schedule_template-csv.R
@@ -1,6 +1,6 @@
 ## code to prepare `schedule_template.csv` dataset as .rda goes here
 
 library(data.table)
-schedule_template = fread('schedule_template.csv')
+schedule_template = fread('data-raw/schedule_template.csv')
 
 usethis::use_data(schedule_template, overwrite = TRUE)
