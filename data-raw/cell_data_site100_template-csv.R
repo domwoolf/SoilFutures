3f488883a9387c3b564c78f4b02f4ffe2357087e
Shelby <shelby@XPS-8930>
2023-08-01 13:58:01 -0400

    downsized site.100 dt

--- data-raw/cell_data_site100_template-csv.R
+++ data-raw/cell_data_site100_template-csv.R
@@ -3,4 +3,6 @@
 library(data.table)
 cell_data_site = fread('data-raw/main_table_extracted_site100.csv')
 
+# OPTIONAL - remove gridid (< 1 ha)
+
 usethis::use_data(cell_data_site, overwrite = TRUE)
