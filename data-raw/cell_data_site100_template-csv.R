58ee58cbf5380c14abfe49ff546bc59b392bbe94
Shelby <shelby@XPS-8930>
2023-10-03 19:45:52 -0400

    fixed irregularities in site.100 table

--- data-raw/cell_data_site100_template-csv.R
+++ data-raw/cell_data_site100_template-csv.R
@@ -4,6 +4,4 @@ library(data.table)
 path           = '/home/shelby/Documents/projects/SoilFutures'
 cell_data_site = fread(paste(path, 'data-raw/main_table_extracted_site100.csv', sep = '/'))
 
-# OPTIONAL - remove gridid (< 1 ha)
-
 usethis::use_data(cell_data_site, overwrite = TRUE)
--- data-raw/cell_data_template-csv.R
+++
@@ -1,6 +0,0 @@
-## code to prepare `cell_data_table_csu.csv` dataset as multiple .rda goes here
-
-library(data.table)
-cell_data = fread('data-raw/cell_data_table_csu-22Dec2022.csv')
-
-usethis::use_data(cell_data, overwrite = TRUE)
