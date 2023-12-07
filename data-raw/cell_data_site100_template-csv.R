105b1163d272ec0e1f5a38bedbdbbd42d52dd501
Shelby <shelby@XPS-8930>
2023-12-07 16:04:28 -0500

    modified site100 table

--- data-raw/cell_data_site100_template-csv.R
+++ data-raw/cell_data_site100_template-csv.R
@@ -6,11 +6,11 @@ cell_data_site = fread(paste(path, 'data-raw/main_table_extracted_site100.csv', 
 #---------------------------------------------------------------------------------------------------
 # Filter by >= 1 ha cutoff
 #---------------------------------------------------------------------------------------------------
-load(paste(path, 'data-raw/input_table_by_gridid_crop_irr_05Dec23_1ha-filtered.RData', sep = '/'))
-gridid_f       = unique(main_table[,gridid])
-
-cell_data_site = cell_data_site[gridid %in% gridid_f,]
-fwrite(cell_data_site, paste(path, 'data-raw/main_table_extracted_site100_1ha-filtered.csv', sep = '/'))
-save(cell_data_site, file = paste(path, 'data-raw/main_table_extracted_site100_1ha-filtered.RData', sep = '/'))
+# load(paste(path, 'data-raw/input_table_by_gridid_crop_irr_05Dec23_1ha-filtered.RData', sep = '/'))
+# gridid_f       = unique(main_table[,gridid])
+#
+# cell_data_site = cell_data_site[gridid %in% gridid_f,]
+# fwrite(cell_data_site, paste(path, 'data-raw/main_table_extracted_site100_1ha-filtered.csv', sep = '/'))
+# save(cell_data_site, file = paste(path, 'data-raw/main_table_extracted_site100_1ha-filtered.RData', sep = '/'))
 #---------------------------------------------------------------------------------------------------
 usethis::use_data(cell_data_site, overwrite = TRUE)
